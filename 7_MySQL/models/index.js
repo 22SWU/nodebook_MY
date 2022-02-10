30fce7fb8ce7b13e34d8c66932263fd6cb05df18
Minyoung-Lee <leemy08@naver.com>
2022-02-10 23:10:37 +0900

    익스프레스와 시퀄라이즈 연결

--- 7_MySQL/models/index.js
+++ 7_MySQL/models/index.js
@@ -42,3 +42,6 @@ const config = require('../config/config')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
+db.sequelize = sequelize;
+
+module.exports = db;
