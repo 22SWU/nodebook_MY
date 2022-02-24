c1d969ccfb618567ffc653ff765d89eedb6ec755
Minyoung-Lee <leemy08@naver.com>
2022-02-25 00:40:40 +0900

    app.js main.css 추가

--- 9_ExpressSNS/app.js
+++ 9_ExpressSNS/app.js
@@ -46,6 +46,6 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-app.listen(qpp.get('port'), () => {
+app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 ;})
