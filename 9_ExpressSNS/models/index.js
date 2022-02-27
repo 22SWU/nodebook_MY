d628d9f2245c6983a0ccbaee197161e12c96a7b1
Minyoung-Lee <leemy08@naver.com>
2022-02-28 01:25:10 +0900

    index.js 오류수정

--- 9_ExpressSNS/models/index.js
+++ 9_ExpressSNS/models/index.js
@@ -39,7 +39,6 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV|| 'development';
 const config = require('../config/config') [env];
-const config = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
 const User = require('./user');
