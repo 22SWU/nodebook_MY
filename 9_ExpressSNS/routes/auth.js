b335b5d08bda72c1488c93ed311fdc82b45e80c7
Minyoung-Lee <leemy08@naver.com>
2022-03-09 23:34:24 +0900

    auth.js 내용추가

--- 9_ExpressSNS/routes/auth.js
+++ 9_ExpressSNS/routes/auth.js
@@ -3,3 +3,25 @@ const passport = require('passport');
 const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+    const { email, nick, password } = req.body;
+    try{
+        const exUser = await User.findOne({ wher: {email}});
+        if(exUser) {
+            return res.redirect('/join?error=exist');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            nick, password: hash,
+        });
+        return res.redirect('/');
+    } catch (error){
+        console.error(error);
+        return next(error);
+    }
+});
+
